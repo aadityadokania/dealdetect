578733034a82a6db2687c0016fb11e1fc4c35678
Aaditya <aadityadokania1@gmail.com>
2023-10-05 00:43:13 +0530

    amazon scraper and store feature added

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,18 +1,18 @@
 "use server"
 
 import { revalidatePath } from "next/cache";
-// import Product from "../models/product.model";
-// import { connectToDB } from "../mongoose";
+import Product from "../models/product.model";
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
-// import { User } from "@/types";
+import { User } from "@/types";
 // import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
   
     try {
-    //   connectToDB();
+      connectToDB();
   
       const scrapedProduct = await scrapeAmazonProduct(productUrl);
   
@@ -20,7 +20,7 @@ export async function scrapeAndStoreProduct(productUrl: string) {
   
       let product = scrapedProduct;
   
-    //   const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+      const existingProduct = await Product.findOne({ url: scrapedProduct.url });
   
       if(existingProduct) {
         const updatedPriceHistory: any = [
@@ -37,13 +37,13 @@ export async function scrapeAndStoreProduct(productUrl: string) {
         }
       }
   
-    //   const newProduct = await Product.findOneAndUpdate(
-    //     { url: scrapedProduct.url },
-    //     product,
-    //     { upsert: true, new: true }
-    //   );
+      const newProduct = await Product.findOneAndUpdate(
+        { url: scrapedProduct.url },
+        product,
+        { upsert: true, new: true }
+      );
   
-    //   revalidatePath(`/products/${newProduct._id}`);
+      revalidatePath(`/products/${newProduct._id}`);
     } catch (error: any) {
       throw new Error(`Failed to create/update product: ${error.message}`)
     }
